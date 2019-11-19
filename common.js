00505d2406472de210be4b456be35f7148b0e9d1
zhuge_lam@163.com <zhugelam>
2019-11-19 17:17:05 +0800

    111

--- common.js
+++ common.js
@@ -1,37 +1,36 @@
 // LAM 公共函数
-// const web_url = 'http://lam.xq.com/'                             // 自定义请求URL
-const web_url = 'https://f.xilukeji.com/xqcrm/public/'      // 自定义请求URL
-// 腾讯地图key
+const Config = require('./config.js')
 /**
  * 获取定位并解析地址(腾讯地图)
  * @param success function 回调函数
  */
 function GetLocation(success) {
-	let map_key = 'LKVBZ-BFD24-QECUW-DK6AS-FPY66-C4BEC'
-	wx.getLocation({
-		type: 'wgs84',
-		success(res) {
-			// console.log(res)
-			if (!map_key) {
-				console.log('未设置地图key参数')
-				return false
-			} else {
-				_get('https://apis.map.qq.com/ws/geocoder/v1/', {
-					location: res.latitude + ',' + res.longitude,
-					key     : map_key,
-				}, function (e) {
-					// console.log(e)
-					var address = {
-						lat      : res.latitude,
-						lng      : res.longitude,
-						city     : e.result.address_component.city,
-						recommend: e.result.formatted_addresses.recommend,
-					}
-					success && success(address)
-				})
-			}
-		},
-	})
+    let map_key = Config.Map_Key
+    wx.getLocation({
+        type: 'wgs84',
+        success(res) {
+            // console.log(res)
+            if (!map_key) {
+                console.log('未设置地图key参数')
+                return false
+            } else {
+                _get('https://apis.map.qq.com/ws/geocoder/v1/', {
+                    location: res.latitude + ',' + res.longitude,
+                    key: map_key,
+                }, function(e) {
+                    // console.log(e)
+                    var address = {
+                        lat: res.latitude,
+                        lng: res.longitude,
+                        city: e.result.address_component.city,
+                        recommend: e.result.formatted_addresses.recommend,
+                        address: e.result.address
+                    }
+                    success && success(address)
+                })
+            }
+        },
+    })
 }
 
 /**
@@ -42,34 +41,34 @@ function GetLocation(success) {
  * @param success function 回调方法
  */
 function GetList(that, url, data, success) {
-	data       = data || {}
-	data.page  = that.data.page   // 获取当前页码
-	data.limit = that.data.limit  // 获取每页数量
-	if (!that.data.nothing && that.data.on_off) {
-		// 判断是否有数据，且开关是否打开
-		that.setData({
-			on_off   : false,
-			none_show: false,
-		})
-		_get(url, data, function (res) {
-			if (that.data.page == 1) {
-				var list = []
-			} else {
-				var list = that.data.list
-			}
-			list     = list.concat(res.list)
-			var page = parseInt(that.data.page) + 1
-			that.setData({
-				list     : list,
-				page     : page,
-				total    : res.total,
-				nothing  : res.nothing,
-				none_show: true,
-				on_off   : true,
-			})
-			success && success(res)
-		})
-	}
+    data = data || {}
+    data.page = that.data.page // 获取当前页码
+    data.limit = that.data.limit // 获取每页数量
+    if (!that.data.nothing && that.data.on_off) {
+        // 判断是否有数据，且开关是否打开
+        that.setData({
+            on_off: false,
+            none_show: false,
+        })
+        _get(url, data, function(res) {
+            if (that.data.page == 1) {
+                var list = []
+            } else {
+                var list = that.data.list
+            }
+            list = list.concat(res.list)
+            var page = parseInt(that.data.page) + 1
+            that.setData({
+                list: list,
+                page: page,
+                total: res.total,
+                nothing: res.nothing,
+                none_show: true,
+                on_off: true,
+            })
+            success && success(res)
+        })
+    }
 }
 
 /**
@@ -81,41 +80,48 @@ function GetList(that, url, data, success) {
  * @param complete  function    请求结束后回调
  */
 function _get(url, data, success, fail, complete) {
-	wx.showNavigationBarLoading()
-	// 构造请求参数
-	data = data || {}
-	// 判断缓存中openid
-	if (wx.getStorageSync('openid')) {
-		data.token = wx.getStorageSync('openid')
-	}
-	if (wx.getStorageSync('user_id')) {
-		data.user_id = wx.getStorageSync('user_id')
-	}
-	// 构造get请求
-	wx.request({
-		url   : VerifyURL(url),
-		header: {
-			'content-type': 'application/json',
-		},
-		data  : data,
-		success(res) {
-			if (res.data.code === 0) {
-				Model(res.data.msg)
-				return false
-			} else {
-				success && success(res.data)
-			}
-		},
-		fail(res) {
-			Model(res.errMsg, '', '', function () {
-				fail && fail(res)
-			})
-		},
-		complete(res) {
-			wx.hideNavigationBarLoading()
-			complete && complete(res)
-		},
-	})
+    wx.showNavigationBarLoading()
+    // 构造请求参数
+    data = data || {}
+    // 判断缓存中openid
+    if (wx.getStorageSync('openid')) {
+        data.token = wx.getStorageSync('openid')
+    }
+    if (wx.getStorageSync('user_id')) {
+        data.user_id = wx.getStorageSync('user_id')
+    }
+    // 构造get请求
+    wx.request({
+        url: VerifyURL(url),
+        header: {
+            'content-type': 'application/json',
+        },
+        data: data,
+        success(res) {
+			// console.log(res.data.code)
+            if (res.data.code === 0) {
+                Model(res.data.msg)
+                return false
+            } else if (res.data.code === 88) {
+				// console.log(res.data.code)
+                Toast(res.data.msg,'','',function(){
+					wx.navigateBack()
+				})
+                return false
+            } else {
+                success && success(res.data)
+            }
+        },
+        fail(res) {
+            Model(res.msg, '', '', function() {
+                fail && fail(res)
+            })
+        },
+        complete(res) {
+            wx.hideNavigationBarLoading()
+            complete && complete(res)
+        },
+    })
 }
 
 /**
@@ -127,43 +133,49 @@ function _get(url, data, success, fail, complete) {
  * @param complete  function    请求结束后回调
  */
 function _post(url, data, success, fail, complete) {
-	wx.showNavigationBarLoading()
-	// 构造请求参数
-	data = data || {}
-	// 判断缓存中openid
-	if (wx.getStorageSync('openid')) {
-		data.token = wx.getStorageSync('openid')
-	}
-	if (wx.getStorageSync('user_id')) {
-		data.user_id = wx.getStorageSync('user_id')
-	}
-	wx.request({
-		url   : VerifyURL(url),
-		header: {
-			'content-type': 'application/x-www-form-urlencoded',
-		},
-		method: 'POST',
-		data  : data,
-		success(res) {
-			if (res.data.code === 0) {
-				Model(res.data.msg,'','', function () {
-					fail && fail(res)
+    wx.showNavigationBarLoading()
+    // 构造请求参数
+    data = data || {}
+    // 判断缓存中openid
+    if (wx.getStorageSync('openid')) {
+        data.token = wx.getStorageSync('openid')
+    }
+    if (wx.getStorageSync('user_id')) {
+        data.user_id = wx.getStorageSync('user_id')
+    }
+    wx.request({
+        url: VerifyURL(url),
+        header: {
+            'content-type': 'application/x-www-form-urlencoded',
+        },
+        method: 'POST',
+        data: data,
+        success(res) {
+            if (res.data.code === 0) {
+                Model(res.data.msg, '', '', function() {
+                    fail && fail(res)
+                })
+                return false
+			} else if (res.data.code === 88) {
+				// console.log(res.data.code)
+				Toast(res.data.msg, '', '', function () {
+					wx.navigateBack()
 				})
 				return false
 			} else {
-				success && success(res.data)
-			}
-		},
-		fail(res) {
-			Model(res.errMsg, '', '', function () {
-				fail && fail(res)
-			})
-		},
-		complete(res) {
-			wx.hideNavigationBarLoading()
-			complete && complete(res)
-		},
-	})
+                success && success(res.data)
+            }
+        },
+        fail(res) {
+            Model(res.msg, '', '', function() {
+                fail && fail(res)
+            })
+        },
+        complete(res) {
+            wx.hideNavigationBarLoading()
+            complete && complete(res)
+        },
+    })
 }
 
 /**
@@ -174,17 +186,18 @@ function _post(url, data, success, fail, complete) {
  * @param callback  function    点击确认后回调
  */
 function Model(msg, title, cancel, callback) {
-	wx.showModal({
-		title     : title.toString() || '提示',
-		content   : msg.toString(),
-		showCancel: cancel || false,
-		success   : function (res) {
-			if (res.confirm) {
-				// 点击确认后执行操作
-				typeof callback == "function" && callback();
-			}
-		},
-	});
+    title = title || '提示'
+    wx.showModal({
+        title: title.toString(),
+        content: msg.toString(),
+        showCancel: cancel || false,
+        success: function(res) {
+            if (res.confirm) {
+                // 点击确认后执行操作
+                typeof callback == "function" && callback();
+            }
+        },
+    });
 }
 
 /**
@@ -195,17 +208,17 @@ function Model(msg, title, cancel, callback) {
  * @param callback  function    提示框成功后回调
  */
 function Toast(msg, icon, duration, callback) {
-	wx.showToast({
-		title   : msg.toString(),
-		icon    : icon || 'none',
-		duration: duration || 3000,
-		mask    : true,
-		success() {
-			callback && (setTimeout(function () {
-				callback()
-			}, duration || 3000))
-		},
-	});
+    wx.showToast({
+        title: msg.toString(),
+        icon: icon || 'none',
+        duration: duration || 1500,
+        mask: true,
+        success() {
+            callback && (setTimeout(function() {
+                callback()
+            }, duration || 1500))
+        },
+    });
 }
 
 /**
@@ -214,57 +227,57 @@ function Toast(msg, icon, duration, callback) {
  * @return {string}
  */
 function VerifyURL(url) {
-	var reg = /^((ht|f)tps?):\/\/[\w\-]+(\.[\w\-]+)+([\w\-.,@?^=%&:\/~+#]*[\w\-@?^=%&\/~+#])?$/;
-	// 判断是否是完整http|https|ftp地址
-	if (reg.test(url)) {
-		// 直接请求当前地址
-		return url
-	} else {
-		// 自定义方法短链接,拼接app.js中设置的请求链接
-		return web_url + url
-	}
+    var reg = /^((ht|f)tps?):\/\/[\w\-]+(\.[\w\-]+)+([\w\-.,@?^=%&:\/~+#]*[\w\-@?^=%&\/~+#])?$/;
+    // 判断是否是完整http|https|ftp地址
+    if (reg.test(url)) {
+        // 直接请求当前地址
+        return url
+    } else {
+        // 自定义方法短链接,拼接app.js中设置的请求链接
+        return Config.Web_Url + url
+    }
 }
 
 //验证手机
 function checkmobile(mobile) {
-	if (!mobile) {
-		Toast('请输入手机号码');
-		return false;
-	}
-	if (!checkTel(mobile)) {
-		Toast('请输入正确的手机号码');
-		return false;
-	}
-	return true;
+    if (!mobile) {
+        Toast('请输入手机号码');
+        return false;
+    }
+    if (!checkTel(mobile)) {
+        Toast('请输入正确的手机号码');
+        return false;
+    }
+    return true;
 }
 
 //检测手机号正确性
 function checkTel(tel) {
-	return /^(0|86|17951)?1[0-9]{10}$/.test(tel);
+    return /^(0|86|17951)?1[0-9]{10}$/.test(tel);
 }
 
 /**
  * 判断字符串是否在数组中
  */
 function in_array(stringToSearch, arrayToSearch) {
-	for (var s = 0; s < arrayToSearch.length; s++) {
-		var thisEntry = arrayToSearch[s].toString()
-		if (thisEntry == stringToSearch) {
-			return true
-		}
-	}
-	return false
+    for (var s = 0; s < arrayToSearch.length; s++) {
+        var thisEntry = arrayToSearch[s].toString()
+        if (thisEntry == stringToSearch) {
+            return true
+        }
+    }
+    return false
 }
 
 // 模块化方法
 module.exports = {
-	GetLocation: GetLocation,   // 获取定位并解析地址
-	GetList    : GetList,       // 获取分页列表信息
-	_get       : _get,          // 通用GET请求
-	_post      : _post,         // 通用POST请求
-	Model      : Model,         // 错误信息提示对话框
-	Toast      : Toast,         // 纯文字弹窗信息
-	VerifyURL  : VerifyURL,     // 判断请求地址，并为方法地址添加http
-	checkmobile: checkmobile,
-	in_array   : in_array,
+    GetLocation: GetLocation, // 获取定位并解析地址
+    GetList: GetList, // 获取分页列表信息
+    _get: _get, // 通用GET请求
+    _post: _post, // 通用POST请求
+    Model: Model, // 错误信息提示对话框
+    Toast: Toast, // 纯文字弹窗信息
+    VerifyURL: VerifyURL, // 判断请求地址，并为方法地址添加http
+    checkmobile: checkmobile,
+    in_array: in_array,
 }
---
+++ weixin.js
@@ -0,0 +1,188 @@
+// 微信相关函数 whm
+const common = require('./common.js')
+const qiniuUploader = require("./qiniuUploader.js")
+const Config = require('./config.js')
+
+// 初始化七牛相关参数
+function initQiniu() {
+    var options = {
+        region: 'ECN', // 华东区
+        uptokenURL: Config.Web_Url + 'index/index/getToken',
+        domain: 'images2',
+        // uploadURL: 'http://images2.img.xilukeji.com',
+        uploadURL: Config.Img_Url,
+        shouldUseQiniuFileName: false,
+    }
+    qiniuUploader.init(options)
+}
+
+module.exports = {
+    chooseImg: chooseImg, //微信上传图片
+    getToken: getToken, //微信上传图片
+    previewImg: previewImg, //微信预览图片
+    scanCode: scanCode, //扫码
+    wxpay: wxpay, //微信支付
+}
+//上传图片
+var tempFilePaths
+var callback
+
+function chooseImg(num, that, uploadtoken, cb) {
+    num = num || 9
+    callback = cb || ''
+    var date = new Date()
+    var year = date.getFullYear()
+    var month = date.getMonth() + 1
+    month = (month < 10 ? "0" + month : month)
+    var strDate = date.getDate()
+    var mydate = (year.toString() + month.toString() + strDate.toString())
+    wx.chooseImage({
+        count: num, // 默认9
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+        success: function(res) {
+            // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+            var tempFilePaths = res.tempFilePaths
+            // console.log(tempFilePaths)
+            for (var i = 0; i < tempFilePaths.length; i++) {
+                var filePath = tempFilePaths[i]
+                var upload_name = 'xqcrm/' + mydate + '/' + filePath.substr(-40, 40)
+                // uploadImg(tempFilePaths);
+                // 交给七牛上传
+                qiniuUploader.upload(filePath, (res) => {
+                        // var picArr=that.picArr||[];
+                        // picArr.push(Config.Img_Url+res.key);
+                        // that.setData({
+                        //   picArr: picArr
+                        // });
+                        //callback方法
+                        if (typeof callback == "function") {
+                            callback(Config.Img_Url + '/' + res.key)
+                        }
+                    }, (error) => {
+                        console.error('error: ' + JSON.stringify(error))
+                    }, {
+                        region: 'ECN', // 华东区
+                        uptoken: uploadtoken,
+                        domain: 'images',
+                        shouldUseQiniuFileName: false,
+                        key: upload_name,
+                    }, // 可以使用上述参数，或者使用 null 作为参数占位符
+                    (progress) => {
+                        // console.log('上传进度', progress.progress)
+                        // console.log('已经上传的数据长度', progress.totalBytesSent)
+                        // console.log('预期需要上传的数据总长度', progress.totalBytesExpectedToSend)
+                    }, cancelTask => that.setData({
+                        cancelTask
+                    }),
+                )
+            }
+        },
+    })
+}
+
+//获取七牛token
+function getToken(num, that, cb) {
+    wx.request({
+        url: Config.Web_Url + 'index/index/getToken',
+        data: {},
+        header: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        method: "POST",
+        dataType: "json",
+        fail: function(e) {
+            console.log('上传失败')
+        },
+        success: function(ret) {
+            // console.log(ret)
+            chooseImg(num, that, ret.data.upToken, cb)
+        },
+    })
+}
+
+function uploadImg(tempFilePaths) {
+    var tempFile = tempFilePaths.shift() //把数组的第一个元素从其中删除，并返回第一个元素的值
+    var url = Config.Web_Url + "index.php?s=Home/Upload/upload"
+    wx.showLoading({
+        title: '上传中...',
+    })
+    wx.uploadFile({
+        url: url,
+        filePath: tempFile,
+        name: 'pic',
+        header: {
+            "Content-Type": "multipart/form-data"
+        },
+        success: function(res) {
+            var ret = JSON.parse(res.data)
+            if (ret.status) {
+                var len = tempFilePaths.length
+                typeof callback == 'function' && callback(ret, len) //图片上传成功后置操作
+                if (len > 0) {
+                    uploadImg(tempFilePaths)
+                } else {
+                    wx.hideLoading()
+                }
+            } else {
+                common.Toast("上传失败，请重新上传")
+                console.log(ret.info)
+            }
+        },
+    })
+}
+
+//预览图片
+function previewImg(current, urls) {
+    wx.previewImage({
+        current: current, // 当前显示图片的http链接
+        urls: urls, // 需要预览的图片http链接列表
+    })
+}
+
+//微信扫码
+function scanCode(cb, onlyFromCamera = false, scanType) {
+    var sType = ['qrCode', 'barCode', 'datamatrix', 'pdf417']
+    if (typeof scanType == "undefined") {
+        scanType = sType
+    }
+    wx.scanCode({
+        onlyFromCamera: onlyFromCamera,
+        scanType: scanType,
+        success: (res) => {
+            if (res.errMsg == "scanCode:ok") {
+                typeof cb == "function" && cb(res)
+            } else {
+                common.Toast(res.errMsg)
+            }
+        },
+    })
+}
+
+//微信支付
+function wxpay(data, cb1, cb2) {
+    wx.requestPayment({
+        timeStamp: data.timeStamp,
+        nonceStr: data.nonceStr,
+        package: data.package,
+        signType: data.signType,
+        paySign: data.paySign,
+        success: function(res) {
+            if (res.errMsg == "requestPayment:ok") {
+                typeof cb1 == "function" && cb1(res)
+            }
+        },
+        fail: function(res) {
+            if (res.errMsg == "requestPayment:fail cancel") {
+                typeof cb2 == "function" && cb2(res)
+            } else {
+                common.Toast("唤起微信支付失败！")
+            }
+        },
+        complete: function(res) {
+            if (res.errMsg == "requestPayment:cancel") {
+                typeof cb2 == "function" && cb2(res)
+            }
+        },
+    })
+}
